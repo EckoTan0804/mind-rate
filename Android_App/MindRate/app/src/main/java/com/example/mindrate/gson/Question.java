c4e4a7f02c1e105d51ff84d2176ac91e30bf8857
EckoTan0804 <ecko0804@gmail.com>
2017-01-08 23:33:57 +0800

    renamed:    MainActivity.java -> activity/MainActivity.java 	new file:   gson/Proband.java 	new file:   gson/Question.java 	new file:   gson/Questionnaire.java

---
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/Question.java
@@ -0,0 +1,13 @@
+package com.example.mindrate.gson;
+
+
+/**
+ * Project: MindRate
+ * Package: com.example.mindrate.gson
+ * Author: Ecko Tan
+ * E-mail: ecko0804@gmail.com
+ * Created at 2017/1/8:23:33
+ */
+
+public class Question {
+}
