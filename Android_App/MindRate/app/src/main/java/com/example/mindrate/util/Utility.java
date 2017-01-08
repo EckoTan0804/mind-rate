93848232bf9eed17f6828898bb0589381f97a0b6
EckoTan0804 <ecko0804@gmail.com>
2017-01-08 23:44:40 +0800

    new file:   fragment/LogInFragment.java 	new file:   util/HttpUtil.java 	new file:   util/Utility.java 	new file:   ../../../../res/layout/fragment_log_in.xml

---
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/util/Utility.java
@@ -0,0 +1,13 @@
+package com.example.mindrate.util;
+
+
+/**
+ * Project: MindRate
+ * Package: com.example.mindrate.util
+ * Author: Ecko Tan
+ * E-mail: ecko0804@gmail.com
+ * Created at 2017/1/8:23:41
+ */
+
+public class Utility {
+}
