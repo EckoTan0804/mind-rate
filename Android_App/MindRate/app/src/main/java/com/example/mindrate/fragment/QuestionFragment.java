78a9c40676e1d7618c115b94520a28657939f113
EckoTan0804 <ecko0804@gmail.com>
2017-01-10 10:45:35 +0100

    new file:   app/src/main/java/com/example/mindrate/activity/ActivityManager.jav	new file:   app/src/main/java/com/example/mindrate/activity/AnswerQuestionActivity.java 	new file:   app/src/main/java/com/example/mindrate/activity/BasicActivity.java 	new file:   app/src/main/java/com/example/mindrate/activity/LogInActivity.java 	renamed:    app/src/main/java/com/example/mindrate/fragment/LogInFragment.java -> app/src/main/java/com/example/mindrate/fragment/QuestionFragment.java 	new file:   app/src/main/java/com/example/mindrate/fragment/chooseQuestionnaireFragment.java 	renamed:    app/src/main/java/com/example/mindrate/service/UploadTask.java -> app/src/main/java/com/example/mindrate/service/AutoDownloadService.java 	renamed:    app/src/main/java/com/example/mindrate/service/DownloadTask.java -> app/src/main/java/com/example/mindrate/service/AutoUploadService.java 	new file:   app/src/main/res/layout/activity_answer_question.xml 	new file:   app/src/main/res/layout/activity_log_in.xml 	new file:   app/src/main/res/layout/fragment_choose_questionnaire.xml 	renamed:    app/src/main/res/layout/fragment_log_in.xml -> app/src/main/res/layout/fragment_question.xml

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/QuestionFragment.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/QuestionFragment.java
@@ -13,12 +13,12 @@ import com.example.mindrate.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link LogInFragment.OnFragmentInteractionListener} interface
+ * {@link QuestionFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LogInFragment#newInstance} factory method to
+ * Use the {@link QuestionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LogInFragment extends Fragment {
+public class QuestionFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class LogInFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public LogInFragment() {
+    public QuestionFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class LogInFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LogInFragment.
+     * @return A new instance of fragment QuestionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LogInFragment newInstance(String param1, String param2) {
-        LogInFragment fragment = new LogInFragment();
+    public static QuestionFragment newInstance(String param1, String param2) {
+        QuestionFragment fragment = new QuestionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class LogInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        return inflater.inflate(R.layout.fragment_question, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
