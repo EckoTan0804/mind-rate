4f98f0614d5b40efefda7c4cc487b02fa4e9fc6c
EckoTan0804 <ecko0804@gmail.com>
2017-01-16 15:02:01 +0100

    finished implementing method sendRequestWithOkHttp

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/util/HttpUtil.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/util/HttpUtil.java
@@ -1,6 +1,9 @@
 package com.example.mindrate.util;
 
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Project: MindRate
  * Package: com.example.mindrate.util
@@ -22,6 +25,8 @@ public class HttpUtil {
      * @param callback provided by okhttp3
      */
     public static void sendRequestWithOkHttp(String address, okhttp3.Callback callback) {
-
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
     }
 }
