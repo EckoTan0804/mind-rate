818572f349b8eb9a0cada36a04a44e502a9bf701
EckoTan0804 <ecko0804@gmail.com>
2017-01-16 15:06:50 +0100

    added attribute activityList and method : addActivity, removeActivity, finishAll

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/ActivityManager.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/ActivityManager.java
@@ -1,6 +1,11 @@
 package com.example.mindrate.activity;
 
 
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Project: MindRate
  * Package: com.example.mindrate.activity
@@ -10,4 +15,22 @@ package com.example.mindrate.activity;
  */
 
 public class ActivityManager {
+
+    public static List<Activity> activityList = new ArrayList<Activity>();
+
+    public static void addActivity(Activity activity) {
+        activityList.add(activity);
+    }
+
+    public static void removeActivity(Activity activity) {
+        activityList.remove(activity);
+    }
+
+    public static void finishAll() {
+        for (Activity activity : activityList) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
 }
