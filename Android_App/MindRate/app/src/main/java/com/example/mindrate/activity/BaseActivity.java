b4b69f8b526948a8f6d6f63f0cbbe9b43da36fa9
EckoTan0804 <ecko0804@gmail.com>
2017-01-16 15:03:24 +0100

    rename BasicActivity to BaseActivity

---
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.mindrate.activity;
+
+import android.support.v4.app.ActivityCompat;
+
+
+/**
+ * Project: MindRate
+ * Package: com.example.mindrate.activity
+ * Author: Ecko Tan
+ * E-mail: ecko0804@gmail.com
+ * Created at 2017/1/10:17:26
+ */
+
+public class BaseActivity extends ActivityCompat {
+}
