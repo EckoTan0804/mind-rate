fbfa9f80e6cf18e739e783bda3063905b5ce955d
EckoTan0804 <ecko0804@gmail.com>
2017-01-17 22:35:30 +0100

    implementing chooseQuestionnaireFragment.java

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/chooseQuestionnaireFragment.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/chooseQuestionnaireFragment.java
@@ -1,109 +1,62 @@
 package com.example.mindrate.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.mindrate.R;
+import com.example.mindrate.gson.Question;
+import com.example.mindrate.gson.Questionnaire;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.id.list;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link chooseQuestionnaireFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link chooseQuestionnaireFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * This class is for the fragment, in which the proband can choose the questionnaires which are
+ * already triggered but not answered yet
+ *
+ * Project: MindRate
+ * Package: com.example.mindrate.fragment
+ * Author: Ecko Tan
+ * E-mail: eckotan@icloud.com
  */
 public class chooseQuestionnaireFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final int LEVLE_QUESTIONNAIRE = 0;
+    public static final int LEVEL_QUESTION = 1;
 
-    private OnFragmentInteractionListener mListener;
+    //====================== UI - Components ====================
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    // ==========================================================
 
-    public chooseQuestionnaireFragment() {
-        // Required empty public constructor
-    }
+    // ===================== model ==============================
+    private List<String> dataList = new ArrayList<>();
+    private List<Questionnaire> questionnaireList;
+    private List<Question> questionList;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment chooseQuestionnaireFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static chooseQuestionnaireFragment newInstance(String param1, String param2) {
-        chooseQuestionnaireFragment fragment = new chooseQuestionnaireFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_questionnaire, container, false);
-    }
+        View view = inflater.inflate(R.layout.choose_questionnaire, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView= (ListView) view.findViewById(R.id.list_view);
     }
 }
