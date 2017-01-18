e9f43f90070521707f2a3b534ca6f82e96aede1e
EckoTan0804 <ecko0804@gmail.com>
2017-01-19 00:17:21 +0100

    fixed the problem which caused by choose_gender

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/LogInActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/LogInActivity.java
@@ -4,21 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.mindrate.R;
 import com.example.mindrate.gson.Birthday;
 import com.example.mindrate.gson.Proband;
+import com.example.mindrate.util.Utility;
 
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class LogInActivity extends AppCompatActivity implements
-        CompoundButton.OnCheckedChangeListener {
-
+public class LogInActivity extends AppCompatActivity {
 
     // =================== proband ==========================
     private String studyID;
@@ -33,6 +33,7 @@ public class LogInActivity extends AppCompatActivity implements
     private EditText edtTxt_studyID;
     private EditText edtTxt_probandID;
     private DatePicker dtPk_birthday;
+    private RadioGroup rdog_chooseGender;
     private RadioButton rdoBtn_gender_male;
     private RadioButton rdoBtn_gender_famale;
     private EditText edtTxt_occuptaion;
@@ -49,13 +50,14 @@ public class LogInActivity extends AppCompatActivity implements
     private void initView() {
         edtTxt_studyID = (EditText) findViewById(R.id.study_id);
         edtTxt_probandID = (EditText) findViewById(R.id.proband_id);
-        dtPk_birthday = (DatePicker) findViewById(R.id.choose_birthday);
-        rdoBtn_gender_male = (RadioButton) findViewById(R.id.male);
-        rdoBtn_gender_famale = (RadioButton) findViewById(R.id.female);
+
+
         edtTxt_occuptaion = (EditText) findViewById(R.id.occupation);
         btn_probandLogIn = (Button) findViewById(R.id.proband_log_in);
 
         // ================= DatePicker ============================
+        
+        dtPk_birthday = (DatePicker) findViewById(R.id.choose_birthday);
         Calendar calender = Calendar.getInstance(TimeZone.getDefault());
         year = calender.get(Calendar.YEAR);
         month = calender.get(Calendar.MONTH);
@@ -72,19 +74,48 @@ public class LogInActivity extends AppCompatActivity implements
             }
         });
 
-        rdoBtn_gender_male.setOnCheckedChangeListener(this);
-        rdoBtn_gender_famale.setOnCheckedChangeListener(this);
+        // ============================================================
+
+        // =============== gender =====================================
+        rdog_chooseGender = (RadioGroup) findViewById(R.id.choose_gender);
+        rdoBtn_gender_male = (RadioButton) findViewById(R.id.male);
+        rdoBtn_gender_famale = (RadioButton) findViewById(R.id.female);
+
+        rdog_chooseGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch(checkedId){
+                    case R.id.male:
+                        gender = "male";
+                        Toast.makeText(LogInActivity.this, gender, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.female:
+                        gender = "female";
+                        Toast.makeText(LogInActivity.this, gender, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+        });
+
+//        rdoBtn_gender_male.setOnCheckedChangeListener(this);
+//        rdoBtn_gender_famale.setOnCheckedChangeListener(this);
         btn_probandLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // 1. TODO: create Proband instance
                 studyID = edtTxt_studyID.getText().toString();
                 probandID = edtTxt_probandID.getText().toString();
                 occupation = edtTxt_occuptaion.getText().toString();
                 Proband proband = new Proband(studyID, probandID, new Birthday(year, month, day),
-                        gender, occupation
-                );
-                // TODO: create JSON
-                // TODO: download QUestionnaire
+                        gender, occupation);
+                // 2. TODO: create JSON
+                String probandJSON = Utility.createJSON(proband);
+                // 3. TODO: download Questionnaire
+
             }
         });
 
@@ -92,24 +123,26 @@ public class LogInActivity extends AppCompatActivity implements
     }
 
 
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        switch (compoundButton.getId()) {
-            case R.id.male:
-                if (isChecked) {
-                    gender = "male";
+//    @Override
+//    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+//        switch (compoundButton.getId()) {
+//            case R.id.male:
+//                if (isChecked) {
+//                    gender = "male";
 //                    Toast.makeText(LogInActivity.this, this.gender, Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.female:
-                if (isChecked) {
-                    gender = "female";
+//                }
+//                break;
+//            case R.id.female:
+//                if (isChecked) {
+//                    gender = "female";
 //                    Toast.makeText(LogInActivity.this, this.gender, Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
+//                }
+//                break;
+//            default:
+//
+//                break;
+//        }
+//    }
 
-                break;
-        }
-    }
+
 }
