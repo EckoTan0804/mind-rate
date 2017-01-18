89468998c04bc9b1fc63a7854b218d8abab2e80f
EckoTan0804 <ecko0804@gmail.com>
2017-01-18 15:19:46 +0100

    wrote some code in order to test LogInActivity

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/MainActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.mindrate.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.mindrate.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static boolean isLogIn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent;
+        if (!isLogIn) {
+            intent = new Intent(this, LogInActivity.class);
+        } else {
+            intent = new Intent(this, AnswerQuestionActivity.class);
+        }
+
+        startActivity(intent);
+
+    }
+
+    public static boolean isLogIn() {
+        return isLogIn;
+    }
+
+    public static void setIsLogIn(boolean isLogIn) {
+        MainActivity.isLogIn = isLogIn;
     }
 }
