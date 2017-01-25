a5bdeafd0d81355f78d9e0bf1824843419b9c773
EckoTan0804 <ecko0804@gmail.com>
2017-01-25 22:25:38 +0100

    fixed the problem which caused by renaming OverviewActivity

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/OverviewActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/OverviewActivity.java
@@ -39,7 +39,7 @@ public class OverviewActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_answer_question);
+        setContentView(R.layout.activity_overview);
 
         initFromIntent();
         initView();
