07ab6cfe776bff4ac34899701c65b16f6283ec5a
EckoTan0804 <ecko0804@gmail.com>
2017-01-29 22:02:37 +0100

    implements Parcelable

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/sensor/TriggerEvent.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/sensor/TriggerEvent.java
@@ -2,7 +2,10 @@ package com.example.mindrate.sensor;
 
 
 import android.hardware.Sensor;
+import android.os.Parcel;
+import android.os.Parcelable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +16,7 @@ import java.util.List;
  * Created at 2017/1/10:18:37
  */
 
-public class TriggerEvent {
+public class TriggerEvent implements Parcelable {
 
     private List<Sensor> sensorList;
 
@@ -24,4 +27,35 @@ public class TriggerEvent {
     public void setSensorList(List<Sensor> sensorList) {
         this.sensorList = sensorList;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeList(this.sensorList);
+    }
+
+    public TriggerEvent() {
+    }
+
+    protected TriggerEvent(Parcel in) {
+        this.sensorList = new ArrayList<Sensor>();
+        in.readList(this.sensorList, Sensor.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<TriggerEvent> CREATOR = new Parcelable
+            .Creator<TriggerEvent>() {
+        @Override
+        public TriggerEvent createFromParcel(Parcel source) {
+            return new TriggerEvent(source);
+        }
+
+        @Override
+        public TriggerEvent[] newArray(int size) {
+            return new TriggerEvent[size];
+        }
+    };
 }
