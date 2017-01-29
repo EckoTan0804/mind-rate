38e5d88fcbf52c718b1349ec630243c7911b94ed
EckoTan0804 <ecko0804@gmail.com>
2017-01-29 18:49:30 +0100

    renamed:    app/src/main/res/layout/answer_question_page.xml -> app/src/main/res/layout/answer_questionnaire_page.xml

---
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/AnswerQuestionnaireActivity.java
@@ -0,0 +1,13 @@
+package com.example.mindrate.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnswerQuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_questionnaire);
+    }
+}
