340ee559a0f35f7775347e75ada44910924a431a
EckoTan0804 <ecko0804@gmail.com>
2017-01-29 23:34:35 +0100

    implemented some init jobs of this Activity

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/AnswerQuestionnaireActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/AnswerQuestionnaireActivity.java
@@ -2,6 +2,7 @@ package com.example.mindrate.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.mindrate.R;
 import com.example.mindrate.gson.Questionnaire;
@@ -10,14 +11,28 @@ public class AnswerQuestionnaireActivity extends BaseActivity {
 
     private Questionnaire questionnaire;
 
+    // ==================== view components =====================
+    private TextView tv_questionnaireID;
+
+    // ==========================================================
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_questionnaire);
+
+        initFromIntent();
+        initView();
     }
 
     private void initFromIntent() {
         Intent intent = getIntent();
+        this.questionnaire =  intent.getParcelableExtra("questionnaire");
+    }
 
+    private void initView() {
+
+        tv_questionnaireID = (TextView) findViewById(R.id.titl_questionnaireID);
+        tv_questionnaireID.setText(questionnaire.getQuestionnaireID());
     }
 }
