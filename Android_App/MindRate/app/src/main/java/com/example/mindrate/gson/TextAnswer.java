ba3101cc5e6f2af6dd25f83f0831eb7c89f25354
EckoTan0804 <ecko0804@gmail.com>
2017-01-31 12:15:55 +0100

    preliminarily implemented

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/TextAnswer.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/TextAnswer.java
@@ -1,6 +1,12 @@
 package com.example.mindrate.gson;
 
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 /**
  * Project: MindRate
  * Package: com.example.mindrate.gson
@@ -11,4 +17,18 @@ package com.example.mindrate.gson;
 
 public class TextAnswer extends QuestionType {
 
+    @Override
+    public void inflateAnswerView(Context context, ViewGroup layout, ViewGroup.LayoutParams
+            layoutParams) {
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams
+                .MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        EditText mEditText = new EditText(context);
+        mEditText.setTextSize(15);
+        mEditText.setTextColor(Color.BLACK);
+        mEditText.setHint("Please input your answer");
+
+        layout.addView(mEditText, params);
+    }
 }
