b382850e14cb3666eb6e37b39e28e88461ae3647
EckoTan0804 <ecko0804@gmail.com>
2017-01-31 12:20:11 +0100

    preliminarily implemented

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/StepScale.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/StepScale.java
@@ -1,6 +1,13 @@
 package com.example.mindrate.gson;
 
 
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.List;
+
 /**
  * Project: MindRate
  * Package: com.example.mindrate.gson
@@ -11,4 +18,46 @@ package com.example.mindrate.gson;
 
 public class StepScale extends QuestionType {
 
+
+
+    private List<Option> optionlist;
+    private String nextQuestionID;
+
+    public StepScale(List<Option> optionlist) {
+        this.optionlist = optionlist;
+    }
+
+    @Override
+    public void inflateAnswerView(Context context, ViewGroup layout, ViewGroup.LayoutParams
+            layoutParams) {
+
+        RadioGroup radioGroup = new RadioGroup(context);
+
+        // add radioButton into radioGroup
+        for (int i = 0; i < optionlist.size(); i++) {
+            Option option = optionlist.get(i);
+            RadioButton radioButton = new RadioButton(context);
+            radioButton.setId(i);
+            radioButton.setText(option.getContent());
+            radioGroup.addView(radioButton);
+        }
+
+        // set listener for this radioGroup
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                nextQuestionID = optionlist.get(checkedId).getNextQuestionID();
+            }
+        });
+
+        layout.addView(radioGroup);
+    }
+
+    public String getNextQuestionID() {
+        return nextQuestionID;
+    }
+
+    public void setNextQuestionID(String nextQuestionID) {
+        this.nextQuestionID = nextQuestionID;
+    }
 }
