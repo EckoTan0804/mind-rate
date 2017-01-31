0abe84441fc8870c8b99c0bfc284e02e58c23808
EckoTan0804 <ecko0804@gmail.com>
2017-01-31 23:29:16 +0100

    removed abstract and implements Parcelable

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/QuestionType.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/QuestionType.java
@@ -1,17 +1,48 @@
 package com.example.mindrate.gson;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.ViewGroup;
 
 /**
  * Created by Renhan on 2017/1/9.
  */
 
-public abstract class QuestionType {
+public class QuestionType implements Parcelable {
 
-    public abstract void inflateAnswerView(Context context, ViewGroup layout, ViewGroup
+    public  void inflateAnswerView(Context context, ViewGroup layout, ViewGroup
             .LayoutParams
-            layoutParams);
+            layoutParams) {
+
+    }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    public QuestionType() {
+    }
+
+    protected QuestionType(Parcel in) {
+    }
+
+    public static final Parcelable.Creator<QuestionType> CREATOR = new Parcelable
+            .Creator<QuestionType>() {
+        @Override
+        public QuestionType createFromParcel(Parcel source) {
+            return new QuestionType(source);
+        }
+
+        @Override
+        public QuestionType[] newArray(int size) {
+            return new QuestionType[size];
+        }
+    };
 }
