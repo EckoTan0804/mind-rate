eeac7fe7c249fcfdff315bbe59dba3bffd056a7d
EckoTan0804 <ecko0804@gmail.com>
2017-02-01 17:35:13 +0100

    added a new attribut inputAnswer and override getQuestionAnswer

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/TextAnswer.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/TextAnswer.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -19,7 +21,7 @@ import android.widget.LinearLayout;
 
 public class TextAnswer extends QuestionType implements Parcelable {
 
-    private EditText mEditText;
+    private String inputAnswer;
 
     @Override
     public void inflateAnswerView(String questionID, Context context, ViewGroup layout, ViewGroup
@@ -31,18 +33,37 @@ public class TextAnswer extends QuestionType implements Parcelable {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams
                 .MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-        mEditText = new EditText(context);
+        EditText mEditText = new EditText(context);
         mEditText.setTextSize(15);
         mEditText.setTextColor(Color.BLACK);
         mEditText.setHint("Please input your answer");
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                inputAnswer = editable.toString();
+            }
+        });
 
         layout.addView(mEditText, params);
     }
 
     @Override
     public QuestionAnswer getQuestionAnswer() {
-        questionAnswer.setAnswerContent(mEditText.getText().toString());
+        questionAnswer.setAnswerContent(inputAnswer);
         return questionAnswer;
+    }
+
+    public TextAnswer() {
     }
 
     @Override
@@ -52,16 +73,16 @@ public class TextAnswer extends QuestionType implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-    }
-
-    public TextAnswer() {
+        super.writeToParcel(dest, flags);
+        dest.writeString(this.inputAnswer);
     }
 
     protected TextAnswer(Parcel in) {
+        super(in);
+        this.inputAnswer = in.readString();
     }
 
-    public static final Parcelable.Creator<TextAnswer> CREATOR = new Parcelable
-            .Creator<TextAnswer>() {
+    public static final Creator<TextAnswer> CREATOR = new Creator<TextAnswer>() {
         @Override
         public TextAnswer createFromParcel(Parcel source) {
             return new TextAnswer(source);
