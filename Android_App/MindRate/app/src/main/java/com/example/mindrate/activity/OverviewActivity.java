5bce5db20b48e318762355d313917316d1690d79
EckoTan0804 <ecko0804@gmail.com>
2017-02-02 18:45:48 +0100

    moved test data to OverviewActivity

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/OverviewActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/OverviewActivity.java
@@ -23,13 +23,25 @@ import com.example.mindrate.fragment.AboutUsFragment;
 import com.example.mindrate.fragment.ChooseQuestionnaireFragment;
 import com.example.mindrate.fragment.ProbandProfileFragment;
 import com.example.mindrate.fragment.WelcomeFragment;
+import com.example.mindrate.gson.DragScale;
+import com.example.mindrate.gson.MultipleChoice;
+import com.example.mindrate.gson.Option;
 import com.example.mindrate.gson.Proband;
+import com.example.mindrate.gson.Question;
+import com.example.mindrate.gson.Questionnaire;
+import com.example.mindrate.gson.SingleChoice;
+import com.example.mindrate.gson.StepScale;
+import com.example.mindrate.gson.TextAnswer;
+import com.example.mindrate.util.Utility;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class OverviewActivity extends BaseActivity {
 
     private Proband proband;
+    private List<Questionnaire> questionnaireList;
 
     // ==================== View components ==================================
     private DrawerLayout mDrawerLayout;
@@ -38,7 +50,7 @@ public class OverviewActivity extends BaseActivity {
     private TextView tv_title;
     private SensorManager sM;
     private List<Sensor> allSensors;
-//    private TriggerEventManager tEM;
+    //    private TriggerEventManager tEM;
 
     // =======================================================================
 
@@ -52,18 +64,25 @@ public class OverviewActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
 
+
         initFromIntent();
         initView();
+        initTestData();
         sM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         allSensors = sM.getSensorList(Sensor.TYPE_ALL);
-//        tEM =  new TriggerEventManager();
+        //        tEM =  new TriggerEventManager();
 
-//        tv_questionText.setText(Utility.createJSON(this.proband));
+        //        tv_questionText.setText(Utility.createJSON(this.proband));
     }
 
     private void initFromIntent() {
         Intent intent = getIntent();
-        String questionnaire = intent.getStringExtra("questionnaire_JSON");
+
+        // questionnaires
+        String questionnaireJSON = intent.getStringExtra("questionnaire_JSON");
+        this.questionnaireList = Utility.fromJsonToQuestionnaire(questionnaireJSON);
+
+        // proband
         this.proband = (Proband) intent.getParcelableExtra("proband");
     }
 
@@ -96,11 +115,12 @@ public class OverviewActivity extends BaseActivity {
             actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
         }
         navView.setCheckedItem(R.id.nav_profile);
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        navView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.nav_profile:
                         tv_title.setText("Profile");
                         replaceFragment(probandProfileFragment);
@@ -141,19 +161,38 @@ public class OverviewActivity extends BaseActivity {
         transition.commit();
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        switch(requestCode){
-//            case 1:
-//                if (resultCode == RESULT_OK) {
-////                    replaceFragment(chooseQuestionnaireFragment);
-//                }
-//                break;
-//            default:
-//
-//                break;
-//        }
-//    }
+    private Questionnaire removeQuestionnaire(String questionnaireID) {
+        Questionnaire removeQuestionnaire = null;
+        Iterator<Questionnaire> iterator = this.questionnaireList.iterator();
+        while (iterator.hasNext()) {
+            Questionnaire q = iterator.next();
+            if (q.getQuestionnaireID().equals(questionnaireID)) {
+                removeQuestionnaire = q;
+                iterator.remove();
+                break;
+            }
+        }
+        return removeQuestionnaire;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    final String answeredQuestionnaireID = data.getStringExtra("answered " +
+                                                                                   "questionnaire " +
+                                                                                 "ID");
+                    removeQuestionnaire(answeredQuestionnaireID);
+
+
+                }
+                break;
+            default:
+
+                break;
+        }
+    }
 
     // ======================= setters and getters =============================
 
@@ -163,5 +202,59 @@ public class OverviewActivity extends BaseActivity {
 
     public void setProband(Proband proband) {
         this.proband = proband;
+    }
+
+    public List<Questionnaire> getQuestionnaireList() {
+        return questionnaireList;
+    }
+
+    public void setQuestionnaireList(List<Questionnaire> questionnaireList) {
+        this.questionnaireList = questionnaireList;
+    }
+
+    // test data
+    private void initTestData() {
+        questionnaireList = new ArrayList<>();
+
+        Questionnaire questionnaire = new Questionnaire("A", "2017.1.2 14:00", "2017.2.2 14:00");
+        // q1
+        ArrayList<Option> optionList = new ArrayList<>();
+        optionList.add(new Option("At home", "Q3"));
+        optionList.add(new Option("At work", "Q3"));
+        optionList.add(new Option("on the way", "Q2"));
+        Question q1 = new Question("Where are you?", new SingleChoice(optionList), "Q1");
+        questionnaire.addQuestion(q1);
+
+        // q2
+        Question q2 = new Question("Where are you heading to?", new TextAnswer(), "Q2");
+        questionnaire.addQuestion(q2);
+
+        // q3
+        Question q3 = new Question("How are you feeling?", new DragScale(10), "Q3");
+        questionnaire.addQuestion(q3);
+
+        // q4
+        ArrayList<Option> optionArrayList = new ArrayList<>();
+        optionArrayList.add(new Option("Swimming", null));
+        optionArrayList.add(new Option("Reading", null));
+        optionArrayList.add(new Option("Coding", null));
+        optionArrayList.add(new Option("Studying", null));
+        Question q4 = new Question("What's ur hobby?", new MultipleChoice(optionArrayList), "Q4");
+        questionnaire.addQuestion(q4);
+
+        // q5
+        ArrayList<Option> options = new ArrayList<>();
+        options.add(new Option("very bad", null));
+        options.add(new Option("bad", null));
+        options.add(new Option("so so", null));
+        options.add(new Option("good", null));
+        options.add(new Option("very good!", null));
+        Question q5 = new Question("Do you like this app?", new StepScale(options), "Q5");
+        questionnaire.addQuestion(q5);
+
+
+        questionnaireList.add(questionnaire);
+        questionnaireList.add(new Questionnaire("B", "2017.1.2", "2017.2.2"));
+        questionnaireList.add(new Questionnaire("C", "2017.1.3", "2017.2.2"));
     }
 }
--- Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/ChooseQuestionnaireFragment.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/fragment/ChooseQuestionnaireFragment.java
@@ -15,16 +15,8 @@ import com.example.mindrate.R;
 import com.example.mindrate.activity.AnswerQuestionnaireActivity;
 import com.example.mindrate.activity.OverviewActivity;
 import com.example.mindrate.adapter.QuestionnaireAdapter;
-import com.example.mindrate.gson.DragScale;
-import com.example.mindrate.gson.MultipleChoice;
-import com.example.mindrate.gson.Option;
-import com.example.mindrate.gson.Question;
 import com.example.mindrate.gson.Questionnaire;
-import com.example.mindrate.gson.SingleChoice;
-import com.example.mindrate.gson.StepScale;
-import com.example.mindrate.gson.TextAnswer;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,94 +48,54 @@ public class ChooseQuestionnaireFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
-            Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+                Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.choose_questionnaire, container, false);
+            View view = inflater.inflate(R.layout.choose_questionnaire, container, false);
 
-        initTestData();
+            initTestData();
 
-//TODO:        initQuestionnaireList();
+            //TODO:        initQuestionnaireList();
 
-        //        titleText = (TextView) view.findViewById(R.id.title_text);
-        listView = (ListView) view.findViewById(R.id.list_view);
-        adapter = new QuestionnaireAdapter(getContext(), R.layout.questionnaire_item,
-                                           questionnaireList);
-        listView.setAdapter(adapter);
-        return view;
-    }
+            //        titleText = (TextView) view.findViewById(R.id.title_text);
+            listView = (ListView) view.findViewById(R.id.list_view);
+            adapter = new QuestionnaireAdapter(getContext(), R.layout.questionnaire_item,
+                                               questionnaireList);
+            listView.setAdapter(adapter);
+            return view;
+        }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 
-                // 1. get the selected questionnaire instance
-                selectedQuestionnaire = questionnaireList.get(position);
-//                selectedQuestionnaire.sendNotification(getActivity());
+                    // 1. get the selected questionnaire instance
+                    selectedQuestionnaire = questionnaireList.get(position);
+                    //                selectedQuestionnaire.sendNotification(getActivity());
 
-                // 2. put this questionnaire instance into intent
-                Intent intent = new Intent(getActivity(), AnswerQuestionnaireActivity.class);
-                intent.putExtra("questionnaire", selectedQuestionnaire);
+                    // 2. put this questionnaire instance into intent
+                    Intent intent = new Intent(getActivity(), AnswerQuestionnaireActivity.class);
+                    intent.putExtra("questionnaire", selectedQuestionnaire);
 
-                // 3. use this intent to start AnswerQuestionnaireActivity
-                getActivity().startActivityForResult(intent, 1);
-            }
-        });
-    }
+                    // 3. use this intent to start AnswerQuestionnaireActivity
+                    getActivity().startActivityForResult(intent, 1);
+                }
+            });
+        }
 
     private void initQuestionnaireList() {
-        this.questionnaireList = ((OverviewActivity)getActivity()).getQuestionnaireList();
+//        this.questionnaireList = ((OverviewActivity)getActivity()).getQuestionnaireList();
+        OverviewActivity overviewActivity = (OverviewActivity)getActivity();
+        this.questionnaireList = overviewActivity.getQuestionnaireList();
     }
 
 
     // test
     private void initTestData() {
-        questionnaireList = new ArrayList<>();
-        ((OverviewActivity)getActivity()).setQuestionnaireList(questionnaireList);
-
-        Questionnaire questionnaire = new Questionnaire("A", "2017.1.2 14:00", "2017.2.2 14:00");
-        // q1
-        ArrayList<Option> optionList = new ArrayList<>();
-        optionList.add(new Option("At home", "Q3"));
-        optionList.add(new Option("At work", "Q3"));
-        optionList.add(new Option("on the way", "Q2"));
-        Question q1 = new Question("Where are you?", new SingleChoice(optionList), "Q1");
-        questionnaire.addQuestion(q1);
-
-        // q2
-        Question q2 = new Question("Where are you heading to?", new TextAnswer(), "Q2");
-        questionnaire.addQuestion(q2);
-
-        // q3
-        Question q3 = new Question("How are you feeling?", new DragScale(10), "Q3");
-        questionnaire.addQuestion(q3);
-
-        // q4
-        ArrayList<Option> optionArrayList = new ArrayList<>();
-        optionArrayList.add(new Option("Swimming", null));
-        optionArrayList.add(new Option("Reading", null));
-        optionArrayList.add(new Option("Coding", null));
-        optionArrayList.add(new Option("Studying", null));
-        Question q4 = new Question("What's ur hobby?", new MultipleChoice(optionArrayList), "Q4");
-        questionnaire.addQuestion(q4);
-
-        // q5
-        ArrayList<Option> options = new ArrayList<>();
-        options.add(new Option("very bad", null));
-        options.add(new Option("bad", null));
-        options.add(new Option("so so", null));
-        options.add(new Option("good", null));
-        options.add(new Option("very good!", null));
-        Question q5 = new Question("Do you like this app?", new StepScale(options), "Q5");
-        questionnaire.addQuestion(q5);
-
-
-        questionnaireList.add(questionnaire);
-        questionnaireList.add(new Questionnaire("B", "2017.1.2", "2017.2.2"));
-        questionnaireList.add(new Questionnaire("C", "2017.1.3", "2017.2.2"));
+        initQuestionnaireList();
     }
 
 
