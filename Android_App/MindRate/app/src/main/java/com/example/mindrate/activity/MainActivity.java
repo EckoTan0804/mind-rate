17615dbb8480575cd5fc7da2a91705f263ad9937
EckoTan0804 <ecko0804@gmail.com>
2017-02-11 13:32:06 +0100

    finished implemnting start-Animation

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/MainActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/MainActivity.java
@@ -2,49 +2,28 @@ package com.example.mindrate.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.mindrate.R;
 import com.example.mindrate.gson.Proband;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements Animation.AnimationListener {
 
     private static boolean isLogIn = false;
     private Proband proband;
+    private CircleImageView appIcon;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final View view = View.inflate(this, R.layout.activity_main, null);
-        setContentView(view);
+        setContentView(R.layout.activity_main);
 
-        final CircleImageView appIcon = (CircleImageView) view.findViewById(R.id.app_icon_image);
+        appIcon = (CircleImageView) findViewById(R.id.app_icon_image);
 
-        AlphaAnimation alphaAnimation = new AlphaAnimation(0.3f, 1.0f);
-        alphaAnimation.setDuration(5000);
-        view.startAnimation(alphaAnimation);
-        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                redirectTo();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        });
-
-
-
-
+        clockWise();
     }
 
     public static boolean isLogIn() {
@@ -71,5 +50,72 @@ public class MainActivity extends BaseActivity {
             intent = new Intent(this, OverviewActivity.class);
         }
         startActivity(intent);
+    }
+
+
+    // ========================== Animation =====================================
+
+    private void myAnim() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim
+                .move);
+        appIcon.startAnimation(animation);
+        Animation secondAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim
+                .clockwise);
+        appIcon.startAnimation(secondAnimation);
+        secondAnimation.setAnimationListener(this);
+    }
+
+
+    private void clockWise() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim
+                .clockwise);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    private void zoom() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R
+                .anim.zoom);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    private void fade() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    private void blink() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    private void move() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    private void slide() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+        appIcon.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        redirectTo();
     }
 }
