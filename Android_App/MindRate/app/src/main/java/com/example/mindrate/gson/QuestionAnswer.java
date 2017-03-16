da0e3a9fe744f41993ded8755e3ac0b5aebedbd1
EckoTan0804 <ecko0804@gmail.com>
2017-03-16 11:29:09 +0100

    added javadoc

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/QuestionAnswer.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/gson/QuestionAnswer.java
@@ -5,6 +5,8 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
+ * This class aims to model a answer of a question
+ *
  * Project: MindRate
  * <br>Package: com.example.mindrate.gson</br>
  * <br>Author: Ecko Tan</br>
@@ -18,6 +20,10 @@ public class QuestionAnswer implements Parcelable {
     private String questionID;
     private String answerContent;
 
+    /**
+     * Constructor
+     * @param questionID id of the question that the <code>questionAnswer</code> belongs to
+     */
     public QuestionAnswer(String questionID) {
         this.questionID = questionID;
     }
@@ -37,6 +43,8 @@ public class QuestionAnswer implements Parcelable {
     public void setAnswerContent(String answerContent) {
         this.answerContent = answerContent;
     }
+
+    // ===================== Parcelable =====================================================
 
     @Override
     public int describeContents() {
