90a67d4ee6904bdb9d2e84228cf3add709619739
EckoTan0804 <ecko0804@gmail.com>
2017-03-18 23:35:51 +0100

    finished testing QuestionType

---
+++ Android_App/MindRate/app/src/androidTest/java/com/example/mindrate/gson/QuestionTypeTest.java
@@ -0,0 +1,35 @@
+package com.example.mindrate.gson;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * <p>
+ * <br>Project: MindRate</br>
+ * <br>Package: com.example.mindrate.gson</br>
+ * <br>Author: Ecko Tan</br>
+ * <br>E-mail: eckotan@icloud.com</br>
+ * <br>Created at 2017/3/18:23:23</br>
+ * </p>
+ */
+public class QuestionTypeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void equals() throws Exception {
+
+    }
+
+}
