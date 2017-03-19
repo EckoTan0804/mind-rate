4e13ab9d7f4a0d4bfccac35eeae45eccba421d1c
Renhan1014 <uqeae@student.kit.edu>
2017-03-19 19:22:18 +0100

    add testclass for activity

---
+++ Android_App/MindRate/app/src/androidTest/java/com/example/mindrate/activity/OverviewActivityTest.java
@@ -0,0 +1,128 @@
+package com.example.mindrate.activity;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * Created by Renhan on 2017/3/19.
+ */
+@RunWith(AndroidJUnit4.class)
+public class OverviewActivityTest {
+    @Rule
+    public ActivityTestRule<OverviewActivity> overviewActivityRule
+            = new ActivityTestRule<>(OverviewActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onResume() throws Exception {
+
+    }
+
+    @Test
+    public void onNewIntent() throws Exception {
+
+    }
+
+    @Test
+    public void onDestroy() throws Exception {
+
+    }
+
+    @Test
+    public void addQuestionnaireToTriggeredQuestionnaireList() throws Exception {
+
+    }
+
+    @Test
+    public void onActivityResult() throws Exception {
+
+    }
+
+    @Test
+    public void switchLanguageImmediately() throws Exception {
+
+    }
+
+    @Test
+    public void actionBar() throws Exception {
+
+    }
+
+    @Test
+    public void getProband() throws Exception {
+
+    }
+
+    @Test
+    public void setProband() throws Exception {
+
+    }
+
+    @Test
+    public void getAllQuestionnaireList() throws Exception {
+
+    }
+
+    @Test
+    public void setAllQuestionnaireList() throws Exception {
+
+    }
+
+    @Test
+    public void getSelectedQuestionnaire() throws Exception {
+
+    }
+
+    @Test
+    public void setSelectedQuestionnaire() throws Exception {
+
+    }
+
+    @Test
+    public void getTriggeredQuestionnaireList() throws Exception {
+
+    }
+
+    @Test
+    public void setTriggeredQuestionnaireList() throws Exception {
+
+    }
+
+    @Test
+    public void getSelectedQuestionnaireIndex() throws Exception {
+
+    }
+
+    @Test
+    public void setSelectedQuestionnaireIndex() throws Exception {
+
+    }
+
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+}
