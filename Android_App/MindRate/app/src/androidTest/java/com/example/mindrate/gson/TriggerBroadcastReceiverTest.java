5fae9ef9ebce6b5889af4c012840b5117609a261
Renhan1014 <uqeae@student.kit.edu>
2017-03-19 10:56:44 +0100

    add testclass for TriggerBroadcastReceiver

---
+++ Android_App/MindRate/app/src/androidTest/java/com/example/mindrate/gson/TriggerBroadcastReceiverTest.java
@@ -0,0 +1,26 @@
+package com.example.mindrate.gson;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Renhan on 2017/3/19.
+ */
+public class TriggerBroadcastReceiverTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onReceive() throws Exception {
+
+    }
+
+}
