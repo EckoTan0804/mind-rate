53f6e249bd9cfc664068f00de0c781151470fcae
EckoTan0804 <ecko0804@gmail.com>
2017-03-24 10:48:17 +0100

    in method setTriggeredSensorData: check whether triggerEvent is null before set sensor data in questionnaire Answer

--- Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/AnswerQuestionnaireActivity.java
+++ Android_App/MindRate/app/src/main/java/com/example/mindrate/activity/AnswerQuestionnaireActivity.java
@@ -27,7 +27,7 @@ import java.util.Date;
 
 /**
  * This is the activity in which the proband can answer the triggered questionnaire.
- *
+ * <p>
  * <p>
  * <br>Project: MindRate</br>
  * <br>Package: com.example.mindrate.activity</br>
@@ -80,7 +80,7 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
         String probandID = intent.getStringExtra("probandID");
         this.questionnaireAnswer = new QuestionnaireAnswer(this.questionnaire.getQuestionnaireID
                 (), probandID);
-        this.setTriggeredSensorData(this.questionnaireAnswer,this.questionnaire);
+        this.setTriggeredSensorData(this.questionnaireAnswer, this.questionnaire);
     }
 
     /**
@@ -88,7 +88,8 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
      */
     private void initView() {
 
-//        ll_displayAnswerOption = (LinearLayout) findViewById(R.id.activity_answer_questionnaire);
+        //        ll_displayAnswerOption = (LinearLayout) findViewById(R.id
+        // .activity_answer_questionnaire);
 
         tv_questionnaireID = (TextView) findViewById(R.id.title_questionnaireID);
         tv_questionnaireID.setText(questionnaire.getQuestionnaireID());
@@ -157,10 +158,12 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
                         ll_displayAnswerOption.removeAllViews();
 
                         // 2. determine whether nextQuestionID is default or specified
-                        if (!TextUtils.isEmpty(currentQuestion.getQuestionType().getNextQuestionID())) {
+                        if (!TextUtils
+                                .isEmpty(currentQuestion.getQuestionType().getNextQuestionID())) {
                             nextQuestionID = currentQuestion.getQuestionType().getNextQuestionID();
                         } else {
-                            nextQuestionID = this.questionnaire.defaultNextQuestionID(currentQuestion);
+                            nextQuestionID = this.questionnaire
+                                    .defaultNextQuestionID(currentQuestion);
                         }
 
                         if (nextQuestionID != null) {
@@ -169,10 +172,12 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
                             Question nextQuestion = this.questionnaire.getQuestion(nextQuestionID);
 
                             // 4. next question inflates its view
-                            nextQuestion.inflateView(tv_question, this, ll_displayAnswerOption, null);
+                            nextQuestion
+                                    .inflateView(tv_question, this, ll_displayAnswerOption, null);
 
                             // 5. determine whether next question is the last question
-                            //                        if (this.questionnaire.isLastQuestion(nextQuestion)) {
+                            //                        if (this.questionnaire.isLastQuestion
+                            // (nextQuestion)) {
                             //                            setButtonAsSubmit();
                             //                        }
 
@@ -188,11 +193,6 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
                         Toast.makeText(this, R.string.question_not_answered, Toast.LENGTH_LONG)
                                 .show();
                     }
-
-
-
-
-
 
 
                 } else if (this.nextOrSubmit == SUBMIT) {
@@ -312,30 +312,36 @@ public class AnswerQuestionnaireActivity extends BaseActivity implements View.On
                 break;
         }
     }
-//===============set Data of Triggered Sensor=================
-    private void setTriggeredSensorData(QuestionnaireAnswer questionnaireAnswer,Questionnaire questionnaire){
-        if(questionnaire.getTriggerEvent().isAmbientTemperature()){
-            questionnaireAnswer.getSensorValues().put("ambientTemperature",questionnaire
-                    .getAmbientTemperatureValue());
-        }
-        if(questionnaire.getTriggerEvent().isLight()){
-            questionnaireAnswer.getSensorValues().put("light",questionnaire.getLightValue());
-        }
-        if(questionnaire.getTriggerEvent().isPressure()){
-            questionnaireAnswer.getSensorValues().put("pressure",questionnaire
-                    .getPressureValue());
-        }
-        if(questionnaire.getTriggerEvent().isProximity()){
-            questionnaireAnswer.getSensorValues().put("proximity",questionnaire
-                    .getProximityValue());
-        }
-        if(questionnaire.getTriggerEvent().isRelativeHumidity()){
-            questionnaireAnswer.getSensorValues().put("relativeHumidity",questionnaire
-                    .getRelativeHumidityValue());
+
+    //===============set Data of Triggered Sensor=================
+    private void setTriggeredSensorData(QuestionnaireAnswer questionnaireAnswer,
+            Questionnaire questionnaire) {
+        if (questionnaire.getTriggerEvent() != null) {
+
+            if (questionnaire.getTriggerEvent().isAmbientTemperature()) {
+                questionnaireAnswer.getSensorValues().put("ambientTemperature", questionnaire
+                        .getAmbientTemperatureValue());
+            }
+            if (questionnaire.getTriggerEvent().isLight()) {
+                questionnaireAnswer.getSensorValues().put("light", questionnaire.getLightValue());
+            }
+            if (questionnaire.getTriggerEvent().isPressure()) {
+                questionnaireAnswer.getSensorValues().put("pressure", questionnaire
+                        .getPressureValue());
+            }
+            if (questionnaire.getTriggerEvent().isProximity()) {
+                questionnaireAnswer.getSensorValues().put("proximity", questionnaire
+                        .getProximityValue());
+            }
+            if (questionnaire.getTriggerEvent().isRelativeHumidity()) {
+                questionnaireAnswer.getSensorValues().put("relativeHumidity", questionnaire
+                        .getRelativeHumidityValue());
+            }
         }
 
     }
-//=========================
+    //=========================
+
     /**
      * Record questionnaire's answer
      */
